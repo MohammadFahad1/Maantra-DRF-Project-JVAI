1a98edf95d74d5dddb95f30e91ea431b96b108f1
Md. Fahad Monshi <fahad4bangladesh@gmail.com>
2026-02-19 15:43:00 +0600

    Returned back to the previous stage starting from scrash again

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
-from user.views import UserRegistrationAPIView, UserLoginAPIView, ForgotPasswordAPIView, VerifyOTPAPIView, ResetPasswordAPIView, GoogleLogin, FacebookLogin
+from django.urls import path, include
+from user.views import UserRegistrationAPIView, UserLoginAPIView, ForgotPasswordAPIView, VerifyOTPAPIView, ResetPasswordAPIView
 
 urlpatterns = [
     path('register/', UserRegistrationAPIView.as_view(), name='register'),
@@ -7,4 +7,4 @@ urlpatterns = [
     path('forgot_password', ForgotPasswordAPIView.as_view(), name='forgot_password'),
     path('verify_otp', VerifyOTPAPIView.as_view(), name='verify_otp'),
     path('reset_password', ResetPasswordAPIView.as_view(), name='reset_password'),
-]
+    ]
